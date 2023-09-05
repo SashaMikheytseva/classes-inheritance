8c5b517ab19872b1e8bec32ae189126e0b817b79
Sasha Mikheytseva <berezinas21@gmail.com>
2023-09-05 09:15:43 +0300

    character fixed

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,20 +1,21 @@
 export default class Character {
   constructor(name, type) {
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+
+    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+      throw new Error('Некорректное значение имени персонажа');
+    }
+
+    if (!types.includes(type)) {
+      throw new Error('Некорректное значение типа персонажа');
+    }
+
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
     this.attack = undefined;
     this.defence = undefined;
-
-    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
-      throw new Error('Некорректное значение имени персонажа');
-    }
-
-    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-    if (!types.includes(type)) {
-      throw new Error('Некорректное значение типа персонажа');
-    }
   }
 
   levelUp() {
@@ -29,10 +30,9 @@ export default class Character {
   }
 
   damage(points) {
-    if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
-    } else {
+    if (this.health < 0) {
       throw new Error('Персонаж мертв');
     }
+    this.health -= points * (1 - this.defence / 100);
   }
 }
